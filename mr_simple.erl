a119187aedb52ba60fa9375af573ae4476b7ac47
Yann Schwartz <yann@polom.com>
2013-06-03 01:09:30 +0200

    wip...

--- mr_simple.erl
+++ mr_simple.erl
@@ -5,16 +5,20 @@
 
 
 map_reduce(Mapper,Reducer,Seq)->
-	Mapped=[Mapper(Item) || Item <- Seq],
-	lists:foldl(fun({K,Vs}=Item,Acc) -> dict:store(K,Reducer(Vs),Acc) end,
-				dict:new(),
-				lists:flatten(Mapped).
+    Mapped=[Mapper(Item) || Item <- Seq],
+    io:format("Mapped:~p~n",[lists:flatten(Mapped)]),
+    Collected=lists:foldl(fun({K,Vs},Acc) -> dict:update(K,fun(V)->[{K,Vs}|V] end,[],Acc) end,
+                dict:new(),
+                lists:flatten(Mapped)),
+    lists:foldl(fun({K,Vs},Acc)-> dict:store(K,Reducer(K,Vs),Acc) end,
+                dict:new(),
+                Collected).
 
 
 
 
 test_mr()->
-	Input = [<<"Maintenant tu marches dans Paris tout seul parmi la foule">>,
+    Input = [<<"Maintenant tu marches dans Paris tout seul parmi la foule">>,
 <<"Des troupeaux d'autobus mugissants près de toi roulent">>,
 <<"L'angoisse de l'amour te serre le gosier">>,
 <<"Comme si tu ne devais jamais plus être aimé">>,
@@ -25,13 +29,15 @@ test_mr()->
 <<"C'est un tableau pendu dans un sombre musée">>,
 <<"Et quelquefois tu vas la regarder de près">>],
 
-	Mapper= fun(Line)-> 
-					Words =binary:split(Line,?PONCTUATION,[global]),
-					[{W,1} || W <- Words]
-			end,
+    Mapper= fun(Line)-> 
+                    Words =binary:split(Line,?PONCTUATION,[global]),
+                    [{W,1} || W <- Words]
+            end,
 
-	Reducer= fun(Key,Pairs)->
-				{Key,lists:foldl(fun({_,Count},Acc)->Acc+Count end,0,Pairs)},
+    Reducer= fun(Key,Pairs)->
+                io:format("Pairs:~p~n",[Pairs]),
+                {Key,lists:foldl(fun({_,Count},Acc)->Acc+Count end,0,Pairs)}
+             end,
 
-	map_reduce(Mapper,Reducer,Input).
+    map_reduce(Mapper,Reducer,Input).
 
